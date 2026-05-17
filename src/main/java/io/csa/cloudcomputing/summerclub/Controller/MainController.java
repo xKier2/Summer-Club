26038cca767d730dc2f55449384e335698b7aa0f
xKier2 <tongol.kiergabriel@gmail.com>
2026-05-17 11:03:11 +0800

    Fixed Login Functionality

--- src/main/java/io/csa/cloudcomputing/summerclub/Controller/MainController.java
+++ src/main/java/io/csa/cloudcomputing/summerclub/Controller/MainController.java
@@ -3,6 +3,7 @@ package io.csa.cloudcomputing.summerclub.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
